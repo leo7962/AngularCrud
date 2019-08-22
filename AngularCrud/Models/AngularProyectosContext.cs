5b4ea732c019c9b9d19dfe3f2d56ed67d6cba68d
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-08-22 10:34:14 -0500

    Se realiza un Scafolding de la base de datos al proyecto.

---
+++ AngularCrud/Models/AngularProyectosContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AngularCrud.Models
+{
+    public partial class AngularProyectosContext : DbContext
+    {
+        public AngularProyectosContext()
+        {
+        }
+
+        public AngularProyectosContext(DbContextOptions<AngularProyectosContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCities> TblCities { get; set; }
+        public virtual DbSet<TblEmployee> TblEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=proyectos-angular.database.windows.net;Initial Catalog=AngularProyectos;User ID=Leo7962;Password=Nickelback7962;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<TblCities>(entity =>
+            {
+                entity.HasKey(e => e.CityId)
+                    .HasName("PK__tblCitie__F2D21A9625CF601D");
+
+                entity.ToTable("tblCities");
+
+                entity.Property(e => e.CityId).HasColumnName("CityID");
+
+                entity.Property(e => e.CityName)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblEmployee>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId)
+                    .HasName("PK__tblEmplo__7AD04FF137599F26");
+
+                entity.ToTable("tblEmployee");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(6)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
